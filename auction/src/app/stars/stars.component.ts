ff508964cd7d40e8b2a42fd427f6452f498239de
Chenhui CH2 Du <duch2@Lenovo.com>
2018-09-10 15:06:30 +0800

    update

--- auction/src/app/stars/stars.component.ts
+++ auction/src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarsComponent implements OnInit {
 
+  // 表示这个星级评价的组件属性应该又他的父组件传递进来
+  @Input()
+  private rating:number = 0;
+  private stars: boolean[];
+
   constructor() { }
 
   ngOnInit() {
+    this.stars = [];
+    // 根据星级来判断true和false
+    for (let i=1; i<=5; i++){
+      this.stars.push(i > this.rating)
+    }
   }
 
 }
+// export class 
